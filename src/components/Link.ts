3bb14e5f1d6e4ebed4aed8554102704fc0e133ba
Thales <thales.fukuda@gmail.com>
2020-02-18 15:26:18 -0300

    Update link style

--- src/components/Link.ts
+++ src/components/Link.ts
@@ -3,7 +3,10 @@ import ds from '../design/designSystem'
 
 export default styled.a`
   color: ${ds.colors.primary};
+  border-bottom: 2px solid ${ds.colors.primary};
+  cursor: pointer;
   &:hover {
     color: ${ds.colors.secondary};
+    border-color: ${ds.colors.secondary};
   }
 `
