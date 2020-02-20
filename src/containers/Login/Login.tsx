2e17560012ade9f1b11abd46773e9ae266844ed4
Thales <thales.fukuda@gmail.com>
2020-02-20 17:22:43 -0300

    Add sign up

--- src/containers/Login/Login.tsx
+++ src/containers/Login/Login.tsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react'
 import Button from '../../components/Button'
 import Input from '../../components/Input'
 import Link from '../../components/Link'
+
 import { SubTitle } from '../../components/Title'
 
 import Api from '../../Api'
--- src/containers/SignUp/elements.ts
+++ src/containers/SignUp/elements.ts
@@ -1,3 +1,25 @@
 import styled from 'styled-components'
+import Text from '../../components/Text'
 
-export const SignUpContainer = styled.div``
+export const SignUpContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
+
+export const SignUpForm = styled.form`
+  margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  button {
+    margin-top: 30px;
+    align-self: flex-end;
+  }
+`
+
+export const ReturnText = styled(Text)`
+  margin-top: 10px;
+  align-self: flex-end;
+`
