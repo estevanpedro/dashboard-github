3659bf7748bd1fa6e2ac8a3aa330b7b3333f54cf
Luis <luisphelipecode@gmail.com>
2020-03-22 15:48:21 -0300

    Fix merge conflict on Library

--- src/containers/Library/Library.tsx
+++ src/containers/Library/Library.tsx
@@ -1,54 +1,9 @@
 import React from 'react'
 import Title from '../../components/Title'
-<<<<<<< HEAD
-import ListRow from '../../components/ListRow'
-import { ContainerList } from './elements'
-const Library = () => {
-  interface Props {
-    schemeName: string
-    schemeCreator?: string
-    balance: number | any
-    payout: number | any
-    lastTransaction: number | string
-    id: number
-  }
-
-  const [libInfoExample, setLibInfoExample] = useState<Props[]>([
-    {
-      schemeName: 'Mensalidade',
-      schemeCreator: 'Jacinto',
-      balance: 1.0,
-      payout: 0.008,
-      lastTransaction: '01/03/2020',
-      id: 1,
-    },
-    {
-      schemeName: 'Payback 2',
-      schemeCreator: 'Abraham',
-      balance: 0.1,
-      payout: 0.008,
-      lastTransaction: '02/03/2020',
-      id: 2,
-    },
-    {
-      schemeName: 'Signiture ',
-      schemeCreator: 'Albert Einstein',
-      balance: 0.001,
-      payout: 0.008,
-      lastTransaction: '03/03/2020',
-      id: 3,
-    },
-  ])
-
-  const listLibrary = libInfoExample.map((info: Props) => {
-    return ListRow(info)
-  })
-=======
 import { TableContainner, Menu, Containner } from './elements'
 import TableTitles from '../../components/Table/TableTitles'
 import TableOptions from '../../components/Table/TableOptions'
 import { LibraryProps } from './index'
->>>>>>> e0641836de0a685228ea54741a03b60df17255db
 
 const Library = ({
   Tab,
@@ -64,13 +19,9 @@ const Library = ({
   return (
     <Containner>
       <Title>Library</Title>
-<<<<<<< HEAD
-      <ContainerList>{listLibrary}</ContainerList>
-    </div>
-=======
       <Menu>
         <TableOptions
-          props="Balance"
+          props='Balance'
           color={colorBalance}
           onClick={() => {
             setPreference('balance')
@@ -80,7 +31,7 @@ const Library = ({
           }}
         />
         <TableOptions
-          props="Currency"
+          props='Currency'
           color={colorCurrency}
           onClick={() => {
             setPreference('currency')
@@ -90,7 +41,7 @@ const Library = ({
           }}
         />
         <TableOptions
-          props="Last Transaction"
+          props='Last Transaction'
           color={colorTransaction}
           onClick={() => {
             setPreference('lastTransaction')
@@ -101,11 +52,8 @@ const Library = ({
         />
       </Menu>
       <TableTitles />
-      <TableContainner>
-        {Tab(preference)}
-      </TableContainner>
+      <TableContainner>{Tab(preference)}</TableContainner>
     </Containner>
->>>>>>> e0641836de0a685228ea54741a03b60df17255db
   )
 }
 
