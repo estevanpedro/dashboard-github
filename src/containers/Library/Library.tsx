78d2b8cdad41f83e6796993c0d2e997ab70e5f89
Luis <luisphelipecode@gmail.com>
2020-03-22 15:44:05 -0300

    Auto format library.tsx

--- src/containers/Library/Library.tsx
+++ src/containers/Library/Library.tsx
@@ -3,7 +3,6 @@ import Title from '../../components/Title'
 import ListRow from '../../components/ListRow'
 import { ContainerList } from './elements'
 const Library = () => {
-
   interface Props {
     schemeName: string
     schemeCreator?: string
@@ -13,34 +12,32 @@ const Library = () => {
     id: number
   }
 
-  const [libInfoExample, setLibInfoExample] = useState<Props[]>(
-    [
-      {
-        schemeName: 'Mensalidade',
-        schemeCreator: 'Jacinto',
-        balance: 1.0000000,
-        payout: 0.008,
-        lastTransaction: '01/03/2020',
-        id: 1,
-      },
-      {
-        schemeName: 'Payback 2',
-        schemeCreator: 'Abraham',
-        balance: 0.10000000,
-        payout: 0.008,
-        lastTransaction: '02/03/2020',
-        id: 2,
-      },
-      {
-        schemeName: 'Signiture ',
-        schemeCreator: 'Albert Einstein',
-        balance: 0.00100000,
-        payout: 0.008,
-        lastTransaction: '03/03/2020',
-        id: 3,
-      },
-    ]
-  )
+  const [libInfoExample, setLibInfoExample] = useState<Props[]>([
+    {
+      schemeName: 'Mensalidade',
+      schemeCreator: 'Jacinto',
+      balance: 1.0,
+      payout: 0.008,
+      lastTransaction: '01/03/2020',
+      id: 1,
+    },
+    {
+      schemeName: 'Payback 2',
+      schemeCreator: 'Abraham',
+      balance: 0.1,
+      payout: 0.008,
+      lastTransaction: '02/03/2020',
+      id: 2,
+    },
+    {
+      schemeName: 'Signiture ',
+      schemeCreator: 'Albert Einstein',
+      balance: 0.001,
+      payout: 0.008,
+      lastTransaction: '03/03/2020',
+      id: 3,
+    },
+  ])
 
   const listLibrary = libInfoExample.map((info: Props) => {
     return ListRow(info)
@@ -49,10 +46,7 @@ const Library = () => {
   return (
     <div>
       <Title>Library</Title>
-      <ContainerList>
-        {listLibrary}
-      </ContainerList>
-
+      <ContainerList>{listLibrary}</ContainerList>
     </div>
   )
 }
