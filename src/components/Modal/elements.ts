5f0115f8af1f33a96d1101a7390d8b7ddfea5f4b
Thales <thales.fukuda@gmail.com>
2020-03-26 13:55:59 -0300

    Add input error basic handling

--- src/components/Modal/elements.ts
+++ src/components/Modal/elements.ts
@@ -44,7 +44,36 @@ export const Close = styled.a`
 export const IconButton = styled.button``
 
 export const ErrorAlert = styled.div`
-  width: 52px;
-  height: 52px;
+  width: 32px;
+  height: 32px;
   background-color: ${props => props.theme.colors.cancel};
+  margin-right: 10px;
+  border-radius: 50%;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  img {
+    width: 22px;
+    height: 22px;
+  }
+`
+
+export const ErrorInfo = styled.div`
+  width: 320px;
+  height: 60px;
+  background-color: ${props => props.theme.colors.cancel};
+  opacity: 0.8;
+  position: absolute;
+  left: -5px;
+  border-radius: 4px;
+  overflow-y: scroll;
+  padding: 10px;
+  box-sizing: border-box;
+  div {
+    color: ${props => props.theme.colors.contrast};
+    font-size: ${props => props.theme.fontSize.regular};
+    display: flex;
+    align-items: center;
+  }
 `
