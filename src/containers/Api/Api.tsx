a4d044a7e3899e6818e08c301d7dd92240930c07
Estevan <estevanpwr@outlook.com>
2020-04-03 17:29:41 -0300

    testing login and signup

--- src/containers/Api/Api.tsx
+++ src/containers/Api/Api.tsx
@@ -1,12 +1,50 @@
-import React from 'react'
-import Title from '../../components/Title'
+import API_ENDPOINT from 'dotenv'
+import axios from 'axios'
 
 const Api = () => {
-  return (
-    <div>
-      <Title>Api</Title>
-    </div>
-  )
+  const serverUrl = API_ENDPOINT
+
+  const login = async (userData: any) => {
+    return await axios
+      .post(`${serverUrl}/login`,
+        {
+          username: userData.username,
+          password: userData.password
+        }
+      )
+      .then((response: any) => {
+        console.log('got signin response', response)
+        return response
+      })
+      .catch((err: any) => {
+        console.log('error at signin', err.response)
+      })
+  }
+
+  const createUser = async (signUpData: any) => {
+    return await axios
+      .post(`${serverUrl}/registration`,
+        {
+          fullname: signUpData.fullname,
+          email: signUpData.email,
+          username: signUpData.username,
+          password: signUpData.password,
+
+        }
+      )
+      .then((response: any) => {
+        return response
+      })
+      .catch((err: any) => {
+        console.log('error inside createUser', err.response)
+        throw err.response
+      })
+  }
+
+  return {
+    login,
+    createUser,
+  }
 }
 
 export default Api
