e001b9966945a88cb445195629b4ed6b93f226d3
Estevan <estevanpwr@outlook.com>
2020-04-11 11:27:03 -0300

    Fix/ Split Details erros

--- src/containers/SplitDetails/SplitDetails.tsx
+++ src/containers/SplitDetails/SplitDetails.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react'
-import { RouteComponentProps } from '@reach/router'
 import { useSelector, useDispatch } from 'react-redux'
 
 import SplitDetailsTable from './SplitDetailsTable'
@@ -9,132 +8,43 @@ import Api from '../../Api'
 import { RootState } from '../../redux/rootReducer'
 import { setLoading } from '../../redux/ducks/loading'
 
-// TODO // import { updateSplitDetails } from '../../redux/ducks/splitDetails'
-
-const SplitExample = {
-  schemeName: 'Mensalidade',
-  schemeCreator: 'Jacinto',
-  lastTransaction: '1583321650000',
-  currency: 'bitcoin',
-  id: 1,
-  userId: 441,
-  public: true,
-  address: 'zZASDio2daijA2testtestass1sadasdZxza',
-  balance: 0.0145,
-  payout: 0.0008,
-  serviceFee: true,
-  owners: [
-    {
-      address: 'as454axAsaDFAsdedasdaASDasdASD',
-      size: 50,
-      label: 'Estevan Pedro Wisoczynski Reboledo',
-      paid: 0.001,
-    },
-    {
-      address: 'as454axAsaDFAsdedasdaASDasdASD',
-      size: 50,
-      label: 'Alberte Einstein',
-      paid: 0.001,
-    },
-  ],
-}
-
-const historyExample = [
-  {
-    id: 1,
-    amount: '0.50000000',
-    type: 'split',
-    sentTo: 'ASDa2d2adaD1251AScsca23154a2cdascxazzz1910sa', //address
-    created_at: '10:08:10 01/01/2020',
-  },
-  {
-    id: 2,
-    amount: '1.00000000',
-    type: 'split',
-    sentTo: 'ASDa2d2adaD1251AScsca23154a2cdascxazzz1910sa', //address
-    created_at: '15:55:10 05/02/2020',
-  },
-  {
-    id: 3,
-    amount: '-0.50050000',
-    type: 'split',
-    sentTo: 'ASDa2d2adaD1251AScsca23154a2cdascxazzz1910sa', //address
-    created_at: '11:34:10 03/03/2020',
-  },
-  {
-    id: 4,
-    amount: '-1.50050000',
-    type: 'split',
-    sentTo: 'ASDa2d2adaD1251AScsca23154a2cdascxazzz1910sa', //address
-    created_at: '08:34:10 04/03/2020',
-  },
-]
-
-interface Props {
-  schemeId?: string
-}
-
-const SplitDetails = ({ schemeId }: Props & RouteComponentProps) => {
+const SplitDetails = (props: any) => {
+  const dispatch = useDispatch()
+  const [schemeId, setSchemeId] = useState(props.schemeId) // splitId is coming from the Library or from the MyScheme throuth routes
   const [schemeDetails, setSchemeDetails] = useState<any[]>([]) // Need to connect splitDetails to the component...
   const [firstSplit, setFirstSplit] = useState<any[]>([])
+  const { secretToken } = useSelector((state: RootState) => state.auth)
   const [historyDetails, setHistoryDetails] = useState<any[]>([])
 
-  const { secretToken } = useSelector((state: RootState) => state.auth)
-
-  const dispatch = useDispatch()
   useEffect(() => {
     const fetchSchemeDetails = async () => {
-      if (schemeId) {
-        const detailsData = {
-          secretToken,
-          schemeId,
-        }
+      const detailsData = {
+        secretToken,
+        schemeId,
+      }
+      try {
+        dispatch(setLoading(true))
+        const response = await Api.getSchemeDetails(detailsData)
 
-        try {
-          dispatch(setLoading(true))
-          const response = await Api.getSchemeDetails(detailsData)
-          dispatch(setLoading(false))
+        dispatch(setLoading(false))
 
-          setSchemeDetails(response.data)
+        setSchemeDetails(response.data)
+        setFirstSplit(response.data.tree.children[0].children)
+        setHistoryDetails(response.data.transactions)
 
-          if (
-            response.data.tree &&
-            response.data.tree.children[0] &&
-            response.data.tree.children[0].children
-          ) {
-            setFirstSplit(response.data.tree.children[0].children)
-            try {
-              dispatch(setLoading(true))
-              const history = await Api.getHistory({
-                secretToken,
-                address: response.data.tree.address,
-              })
-              dispatch(setLoading(false))
-              setHistoryDetails(history.data)
-            } catch (err) {
-              dispatch(setLoading(false))
-              console.error(err)
-            }
-          }
-        } catch (err) {
-          dispatch(setLoading(false))
-          console.error(err)
-        }
+      } catch (e) {
+        console.error(e)
       }
     }
     fetchSchemeDetails()
-  }, [dispatch, schemeId, secretToken])
+  }, [dispatch, props.splitId])
 
   function createTransList() {
     const Table = ({ info, id }: { info: any; id: number }) => {
       return (
         <>
           <ValuesField pair={id % 2 === 0 ? true : false}>
-            <BalanceText>
-              {info.amount_received > 0
-                ? info.amount_received
-                : '-' + info.amount_sent}
-            </BalanceText>
+            <BalanceText>{info.amount_received > 0 ? info.amount_received : '-' + info.amount_sent}</BalanceText>
             <TableText>{info.created_at}</TableText>
             <TableText>{info.network}</TableText>
           </ValuesField>
@@ -161,12 +71,15 @@ const SplitDetails = ({ schemeId }: Props & RouteComponentProps) => {
                 )}
             </BalanceText>
             <TableText width='25px'>{info.info.percentage * 100}</TableText>
-            <TableText width='100px'>{info.name}</TableText>
+            <TableText width='100px'>
+              {info.name}
+            </TableText>
             <TableText width='40px'></TableText>
           </ValuesField>
         </>
       )
     }
+    console.log('firstSplit: ', firstSplit)
     const Map = firstSplit.map((info: any, id: number) => {
       return <Table info={info} id={id} key={id} />
     })
@@ -176,8 +89,6 @@ const SplitDetails = ({ schemeId }: Props & RouteComponentProps) => {
   return (
     <SplitDetailsTable
       schemeDetails={schemeDetails}
-      SplitExample={SplitExample}
-      historyExample={historyExample}
       createShareList={createShareList}
       createTransList={createTransList}
       firstSplit={firstSplit}
