db7603bab58d9a52288867315fc23160edd9c7b0
Thales <thales.fukuda@gmail.com>
2020-04-11 17:01:27 -0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ const App = (): JSX.Element => (
       <ThemeService>
         <PersistGate persistor={persistor} loading={null}>
           <Router id='router-container'>
-            <Redirect from='/' to='/my-schemes' />
+            <Redirect from='/' to='/login' />
             <Route path='/login' component={Login} />
             <Route path='/sign-up' component={SignUp} />
             <LoggedRoute path='/my-schemes' component={MySchemes} />
