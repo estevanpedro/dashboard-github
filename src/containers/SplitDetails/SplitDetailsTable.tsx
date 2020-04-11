dd802870c51bed84d15ca5b62b9099a388ec2597
Thales <thales.fukuda@gmail.com>
2020-04-11 13:17:55 -0300

    Add back button

--- src/containers/SplitDetails/SplitDetailsTable.tsx
+++ src/containers/SplitDetails/SplitDetailsTable.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
-import { Link } from '@reach/router'
+import { Link, navigate, RouteComponentProps } from '@reach/router'
 
 import QRCode from 'qrcode.react'
+
+import { TextLink } from '../../components'
 
 import {
   Category,
@@ -43,7 +45,7 @@ const SplitDetails = ({
   schemeDetails,
   firstSplit,
   historyDetails,
-}: Props) => {
+}: Props & RouteComponentProps) => {
   let ShareData = (firstSplit: any) => {
     let labels: any[] = []
     let size: any[] = []
@@ -110,8 +112,13 @@ const SplitDetails = ({
     ],
   }
 
+  const handleGoBack = () => {
+    navigate('/my-schemes')
+  }
+
   return (
     <Container>
+      <TextLink onClick={handleGoBack}>← My Schemes</TextLink>
       <Header>
         <Title>{schemeDetails.name}</Title>
         <Category>
