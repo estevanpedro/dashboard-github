3bf72d2d7574b7a795aa8c2794f0ebde2c36a3da
Estevan <estevanpwr@outlook.com>
2020-04-20 10:11:39 -0300

    splitdetails update

--- src/containers/SplitDetails/SplitDetails.tsx
+++ src/containers/SplitDetails/SplitDetails.tsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useContext } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
+import { ThemeContext } from 'styled-components'
 
 import SplitDetailsTable from './SplitDetailsTable'
-import { ValuesField, TableText, BalanceText, CopyButton } from './elements'
+import { ValuesField, TableText, BalanceText, CopyButton, AddressText } from './elements'
 import { FaRegCopy } from "react-icons/fa";
 
 import Api from '../../Api'
@@ -17,7 +18,7 @@ const SplitDetails = (props: any) => {
   const [firstSplit, setFirstSplit] = useState<FirstSplitType[]>([])
   const { secretToken } = useSelector((state: RootState) => state.auth)
   const [historyDetails, setHistoryDetails] = useState<HistoryType[]>([])
-
+  const themeContext = useContext(ThemeContext)
   useEffect(() => {
     const fetchSchemeDetails = async () => {
       const detailsData = {
@@ -46,7 +47,13 @@ const SplitDetails = (props: any) => {
       return (
         <>
           <ValuesField pair={id % 2 === 0 ? true : false}>
-            <BalanceText width='90px'>{info.amount_received > 0 ? info.amount_received : '-' + info.amount_sent}</BalanceText>
+            <BalanceText width='90px' color={info.amount_received > 0 ? themeContext.colors.primary : themeContext.colors.cancel} >
+              {
+                info.amount_received > 0 ?
+                  info.amount_received
+                  : '-' + info.amount_sent
+              }
+            </BalanceText>
             <TableText>{
               new Date(
                 info.created_at * 1000
@@ -69,7 +76,7 @@ const SplitDetails = (props: any) => {
       return (
         <>
           <ValuesField pair={id % 2 === 0 ? true : false}>
-            <BalanceText width='110px'>
+            <AddressText width='110px'>
               {info.address.slice(0, 5) +
                 '...' +
                 info.address.slice(
@@ -79,7 +86,7 @@ const SplitDetails = (props: any) => {
               <CopyButton onClick={() => { navigator.clipboard.writeText(info.address) }} >
                 <FaRegCopy />
               </CopyButton>
-            </BalanceText>
+            </AddressText>
 
 
             <TableText width='25px'>{info.info.percentage * 100}</TableText>
