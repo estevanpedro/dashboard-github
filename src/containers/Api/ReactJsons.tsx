3d6a66da3a5fffe332bd7b20e00355f095590b68
Estevan <estevanpwr@outlook.com>
2020-05-02 18:19:37 -0300

    update ReactJson

--- src/containers/Api/ReactJsons.tsx
+++ src/containers/Api/ReactJsons.tsx
@@ -1,23 +1,23 @@
-import React, {useContext} from 'react'
+import React, { useContext } from 'react'
 import { ThemeContext } from 'styled-components'
 import ReactJson from 'react-json-view'
 
 const ReactJsons = (props: any) => {
-    const themeContext = useContext(ThemeContext)
-    return (
-        <ReactJson
+  const themeContext = useContext(ThemeContext)
+  return (
+    <ReactJson
       theme={
         themeContext.colors.primary === '#FF9140'
           ? 'summerfruit:inverted'
           : 'ashes'
       }
-        src={props}
-        // style={{ backgroundColor: themeContext.colors.secondaryBg }}
-        displayObjectSize={false}
-        displayDataTypes={false}
-        name={false}
-        style={{width: 700, backgroundColor: themeContext.colors.secondaryBg}}
-      />
-    )
+      src={props}
+      enableClipboard={false}
+      displayObjectSize={false}
+      displayDataTypes={false}
+      name={false}
+      style={{ width: 700, backgroundColor: themeContext.colors.secondaryBg }}
+    />
+  )
 }
 export default ReactJsons
